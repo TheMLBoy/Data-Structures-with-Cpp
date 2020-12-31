6d3a1837c26b1873de81a998a0d8e8911c16338b
Muhammad Haris <75581649+mharis4@users.noreply.github.com>
2020-12-31 16:11:58 +0500

    Using Graph functions implemeted in Graph with set.h

---
+++ Graph Data Structures/Graph.cpp
@@ -0,0 +1,29 @@
+//#include"Graph with Set.h"
+//
+//int main()
+//{
+//	Graph g;
+//	g.AddVertex("1");
+//	g.AddVertex("2");
+//	g.AddVertex("3");
+//	g.AddVertex("4");
+//	g.AddVertex("5");
+//	g.AddVertex("6");
+//	g.AddVertex("7");
+//	g.AddVertex("8");
+//	g.AddEdge("1", "2");
+//	g.AddEdge("1", "3");
+//	g.AddEdge("1", "4");
+//	g.AddEdge("4", "3");
+//	g.AddEdge("4", "5");
+//	g.AddEdge("4", "6");
+//	g.AddEdge("4", "7");
+//	g.AddEdge("6", "5");
+//	g.AddEdge("6", "7");
+//	g.Display();
+//	g.BFS("4");
+//	cout << "\nAfter BFS : \n\n";
+//	g.Display_Vertices();
+//	system("pause");
+//	return 0;
+//}
